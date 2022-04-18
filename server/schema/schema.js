7344a29498dff5d64fb05c69bc49877f60136d6b
LingyuLiu11 <linalxly@gmail.com>
2022-04-18 04:22:56 -0400

    change to graphqlID

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,7 +3,7 @@ const graphql = require('graphql');
 const _ = require('lodash');
 //describe object type 
 //first grasp variable from graphql package
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID} = graphql;
 
 
 // dummy data:
@@ -21,7 +21,7 @@ var books = [
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
     })
@@ -32,7 +32,7 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: { id: { type: GraphQLString } }, //get book by id
+            args: { id: { type: GraphQLID } }, //get book by id
             resolve(parent, args) {
                 //code to get data from db
                 //using dummy data:
