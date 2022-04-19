3b4f6c5d6866e8aaa5dea78fdf90cde12e6983a7
LingyuLiu11 <linalxly@gmail.com>
2022-04-18 23:36:36 -0400

    add mongodb

--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require('./schema/schema');
+
+const mongoose = require('mongoose');
+
+
 const app = express();
+
+mongoose.connect('mongodb+srv://spinoza:Spinoza123@cluster1.epqkp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connection.once('open', () => {
+    console.log('connected to db');
+})
 
 app.use("/graphql", graphqlHTTP({
     schema: schema,
