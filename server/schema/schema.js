ad37581b7d678ed873da6801da633cff17226d93
LingyuLiu11 <linalxly@gmail.com>
2022-04-19 00:11:06 -0400

    add book mutation L19

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -119,6 +119,24 @@ const Mutation = new GraphQLObjectType({
                 });
             return author.save();//save to db
             }
+        },
+
+        addBook: {
+            type: BookType,
+            args: {
+                name: {type: GraphQLString},
+                genre: {type: GraphQLString},
+                authorId: {type: GraphQLID}
+            },
+            resolve(parent, args) {
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                })
+
+                return book.save();
+            }
         }
     }
 })
