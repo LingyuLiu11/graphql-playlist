6e172c2aca20d8b1fe4a8e0f2336c45683520fa9
LingyuLiu11 <linalxly@gmail.com>
2022-04-19 05:11:41 -0400

    add css

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -25,7 +25,7 @@ class BookList extends Component {
         
     }
     render(){
-        console.log(this.props);
+        console.log("list" + this.props.books);
         return(
             <div>
                 <ul id="book-list">
